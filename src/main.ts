bcbe26d21b37d14f9419a9094e0364028c42fe2d
zhangxinxin <1285367184@qq.com>
2024-06-29 23:36:49 +0800

    feat: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import App from './App.vue';
 import { setupStores } from './stores';
 import { setupRouter } from './router';
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
 
 const bottstrap = () => {
   const app = createApp(App);
